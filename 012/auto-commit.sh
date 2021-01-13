42e831dbe845588ed53eaf38556fa77f066e20db
Xvezda <xvezda@naver.com>
2021-01-13 18:33:28 +0900

    Update auto-commit.sh

--- 012/auto-commit.sh
+++ 012/auto-commit.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-git log --pretty='format:%s' --grep='^Add day' \
+DAY="$(git log --pretty='format:%s' --grep='^Add day' \
     | cut -d ' ' -f 3 \
     | xargs -I{} echo {}+1 \
-    | bc | head -n 1 \
-    | xargs -I{} echo 'Add day {}'
+    | bc | head -n 1)"
+PAD_SIZE=3
+PADDED="$(printf "%0${PAD_SIZE}d" $DAY)"
 
+cd -- $(git rev-parse --show-toplevel)
+if [[ ! -e "$PADDED" ]]; then
+    >&2 echo "challenge for today not found"
+    exit 1
+fi
+
+git add $PADDED
+git commit -m "Add day $DAY"
