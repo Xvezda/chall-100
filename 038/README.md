2eaf900765cbe244c28d07f73ac6804f1146a880
Xvezda <xvezda@naver.com>
2021-02-07 22:59:43 +0900

    Add day 38

---
+++ 038/README.md
@@ -0,0 +1,18 @@
+# Day 38
+
+Web app to save clipboard content as file.
+
+## References
+
+* https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Functions/Method_definitions
+* https://stackoverflow.com/a/21016088
+* https://developer.mozilla.org/ko/docs/Web/CSS/CSS_Flexible_Box_Layout/Flexbox%EC%9D%98_%EA%B8%B0%EB%B3%B8_%EA%B0%9C%EB%85%90
+* https://stackoverflow.com/a/497108
+* https://developer.mozilla.org/ko/docs/Web/API/EventTarget/addEventListener
+* https://developer.mozilla.org/en-US/docs/Web/API/Node/contains
+* https://developer.mozilla.org/en-US/docs/Web/API/Clipboard/read
+* https://developer.mozilla.org/en-US/docs/Web/API/Blob
+* https://developer.mozilla.org/en-US/docs/Web/API/ClipboardEvent/ClipboardEvent
+* https://developer.mozilla.org/en-US/docs/Web/API/Clipboard_API
+* https://developer.mozilla.org/en-US/docs/Web/API/Node/removeChild
+
---
+++ 038/app.js
@@ -0,0 +1,184 @@
+class App {
+  constructor(parent) {
+    this.parent = parent
+    this.setupInterface()
+
+    // Event names to attach focus listener
+    !['load', 'focus'].forEach(e => {
+      window.addEventListener(e, this.onFocus.bind(this), false)
+    })
+    window.addEventListener('contextmenu', this.onContextMenu.bind(this), false)
+    window.addEventListener('click', this.onClick.bind(this), true)
+  }
+
+  setupInterface() {
+    this.hidden = document.createElement('input')
+    this.hidden.type = 'text'
+    this.hidden.style.position = 'absolute'
+    this.hidden.style.top = '-100%'
+
+    this.hidden.addEventListener('paste', this.onPaste.bind(this), false)
+
+    this.container = document.createElement('div')
+    this.container.style.width = '100vw'
+    this.container.style.height = '100vh'
+    this.container.style.display = 'flex'
+    this.container.style.alignItems = 'center'
+    this.container.style.justifyContent = 'center'
+
+    this.parent.appendChild(this.hidden)
+
+    this.button = document.createElement('button')
+    this.button.textContent = 'Download'
+    this.button.style.padding = '20px'
+    this.button.style.fontSize = '32px'
+
+    this.button.addEventListener('click', this.onDownload.bind(this), false)
+
+    this.disableButton()
+
+    this.container.appendChild(this.button)
+    this.parent.appendChild(this.container)
+
+  }
+
+  disableButton() {
+    this.button.setAttribute('disabled', 'disabled')
+  }
+
+  enableButton() {
+    this.button.removeAttribute('disabled')
+  }
+
+  set item(value) {
+    if (!value) {
+      delete this._item
+      this.disableButton()
+
+      return
+    }
+    this._item = value
+    this.enableButton()
+  }
+
+  get item() {
+    if ('_item' in this) {
+      return this._item
+    }
+    return
+  }
+
+  removeMenu() {
+    try {
+      this.parent.removeChild(this.menu)
+    } catch (e) {}
+  }
+
+  onDownload(event) {
+    if (!this.item) return
+
+    const link = document.createElement('a')
+
+    link.setAttribute('download', 'clipboard')
+    link.href = this.item
+    link.click()
+
+    this.item = undefined
+  }
+
+  async onPaste(event) {
+    event.preventDefault()
+    console.log(event)
+
+    const result = await navigator.permissions.query({name: 'clipboard-read'})
+    if (result.state !== 'granted' && result.state !== 'prompt') return
+
+    const data = await navigator.clipboard.read()
+    // TODO: Zip files?
+    data.forEach(item => {
+      item.types.forEach(async (type) => {
+        const blob = await item.getType(type)
+        this.item = URL.createObjectURL(blob)
+
+        /*
+        if (type.startsWith('image/')) {
+          const blob = await item.getType(type)
+
+          const img = new Image()
+          img.src = URL.createObjectURL(blob)
+
+          this.parent.appendChild(img)
+        }
+        */
+      })
+    })
+  }
+
+  onClick(event) {
+    event.preventDefault()
+    console.log(event)
+
+    if ('menu' in this
+        && !(event.target === this.menu || this.menu.contains(event.target))) {
+      this.removeMenu()
+    }
+    this.onFocus()
+  }
+
+  onFocus(event) {
+    console.log(event)
+    console.dir(document.activeElement)
+
+    const active = document.activeElement
+    switch (active.tagName) {
+      case 'INPUT':
+      case 'TEXTAREA':
+        break
+      default:
+        break
+    }
+    this.hidden.focus()
+  }
+
+  onContextMenu(event) {
+    event.preventDefault()
+    console.log(event)
+
+    this.removeMenu()
+    const menu = (() => {
+      if ('menu' in this) {
+        return this.menu
+      }
+      const menu = document.createElement('div')
+      menu.style.position = 'absolute'
+      menu.style.border = '1px solid darkgray'
+
+      const menuPaste = document.createElement('button')
+      menuPaste.textContent = 'paste'
+      menuPaste.style.padding = '10px'
+      menuPaste.style.background = 'transparent'
+      menuPaste.style.border = '0'
+
+      menuPaste.onclick = (evt) => {
+        console.log('clicked', evt)
+        // Mimic paste event
+        const clipboardEvent = new ClipboardEvent('paste')
+        this.onPaste(clipboardEvent)
+
+        this.removeMenu()
+      }
+
+      menu.appendChild(menuPaste)
+
+      return menu
+    })()
+
+    this.menu = menu
+    this.menu.style.top = `${event.clientY}px`
+    this.menu.style.left = `${event.clientX}px`
+
+    this.parent.appendChild(this.menu)
+  }
+}
+
+new App(document.body)
